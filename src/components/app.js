fba28b4b6b8ee9d830da626d0b93acc31f864d04
Aayush Shah <shahaayush@outlook.com>
2016-11-26 06:04:21 +0530

    Done with basic view still left with functionality and good UI

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import TodosList from './todos-list';
 
+const todos =[
+
+{
+	task: 'make React',
+	isCompleted: false
+},
+{
+	task: 'eat dinner',
+	isCompleted: true
+}
+];
 
 export default class App extends React.Component {
+	
+	constructor(props) {
+        super(props);
+
+        this.state = {
+            todos
+        };
+}
+
+
 	render() {
 		return (
 			<div>
 			<h1>React ToDos App by Aayush Shah !!</h1>
+			<TodosList todos={this.state.todos} />
 			</div>
 			);
 	}
---
+++ src/components/todos-list.js
@@ -0,0 +1,24 @@
+import _ from 'lodash';
+import React from 'react';
+import ToDosListHeader from './todos-list-header';
+import TodosListItem from './todos-list-item';
+
+export default class ToDosList extends React.Component {
+	renderItems(){
+		return _.map(this.props.todos, (todo, index) => <TodosListItem key={index
+			} {...todo} />);
+	}
+
+
+	render() {
+
+		return (
+			<table>
+				<ToDosListHeader />
+				<tbody>
+					{this.renderItems()}
+				</tbody>
+			</table>
+			);
+	}
+}
